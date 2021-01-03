c3e3b52196862d3d57426fd5eca2a883c562c0b3
Cyrill Meier <cyrill.meier@student.fhnw.ch>
2021-01-03 13:48:37 +0100

    Text change

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ function App() {
         </ReactBootStrap.Form>
       </ReactBootStrap.Navbar>
       <hr></hr>
-      <h1>Teile dein Bilder</h1>
+      <h1>Teile deine Bilder</h1>
 
         <input
           onChange={e => setFormData({ ...formData, 'name': e.target.value})}
